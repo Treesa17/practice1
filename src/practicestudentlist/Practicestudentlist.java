ab6c6bf899120baf8dd270b11f83368dff84d3c9
Treesa Saju <122334012+Treesa17@users.noreply.github.com>
2023-02-12 20:48:17 -0500

    Update Practicestudentlist.java

--- src/practicestudentlist/Practicestudentlist.java
+++ src/practicestudentlist/Practicestudentlist.java
@@ -27,6 +27,7 @@ public class Practicestudentlist {
         for(int i=0;i<list.length;i++){
             System.out.println(list[i].getName()+list[i].getAge()); 
                 System.out.println("Treeesa");
+              System.out.println("Last Name Saju");
         }
         
         
