30f0f133faf757a011a68aec37da7432c1d741c0
Treesa17 <sajut@sheridancollege.ca>
2023-02-12 21:26:09 -0500

    Student List practice1

---
+++ src/practicestudentlist/PartTime.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicestudentlist;
+
+/**
+ *
+ * @author trees
+ */
+public class PartTime {
+   private String pay;
+
+    public String getPay() {
+        return pay;
+    }
+
+    public void setPay(String pay) {
+        this.pay = pay;
+    }
+   
+    
+}
